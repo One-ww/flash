48fa924149cd918005ed52b501e8e3c758809205
flash <1918697599@qq.com>
2024-12-16 17:36:28 +0800

    小修改

--- scripts/constant.js
+++ scripts/constant.js
@@ -1,13 +1,13 @@
 // 一些公共常量
-const path = require('path');
+const path = require('path')
 
-const ROOT_PATH = path.resolve(__dirname, '../');
+const ROOT_PATH = path.resolve(__dirname, '../')
 
-const SERVER_HOST = 'localhost';
-const SERVER_PORT = 8080;
+const SERVER_HOST = 'localhost'
+const SERVER_PORT = 8080
 
 module.exports = {
   ROOT_PATH,
   SERVER_HOST,
   SERVER_PORT
-};
+}
